fe50534cd6f4d951ed37ad5d3beae8049fd010a2
Carlos Daniel <carlosdaniel.londono@utp.edu.co>
2021-10-05 15:21:04 -0500

    Perfecto

--- php_lexical_analyzer/code.php
+++ php_lexical_analyzer/code.php
@@ -1,11 +1,14 @@
 <?php
+# Este es un comentario de una linea
 function RecursiveBinarySearch($array, $search, $left, $right)
+// Este es otro comentario de una linea
 {
     if ($left > $right)
     {
         return -1;
     }
-
+    /* Este es un comentario de
+    varias lineas*/
     $average_element_index = floor(($left + $right) / 2);
     $average_element = $array[$average_element_index];
 
